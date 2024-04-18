f4c8705c776254d69f7282c846e2542e008e03b9
MroziuOfficial <kubamaciekm22@gmail.com>
2024-04-18 18:15:36 +0200

    Initial commit

---
+++ Controllers/AnimalsController.cs
@@ -0,0 +1,68 @@
+﻿using apbd_cw06.Models;
+using apbd_cw06.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace apbd_cw06.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AnimalsController : ControllerBase
+{
+    private readonly IConfiguration _config;
+    public AnimalsController(IConfiguration config)
+    {
+        _config = config;
+    }
+    
+    [HttpGet]
+    public IActionResult GetAnimals()
+    {
+        //otwieramy polaczenie do bazy
+        SqlConnection conn = new SqlConnection(_config.GetConnectionString("Default"));
+        conn.Open();
+        //definicja commanda
+        SqlCommand command = new SqlCommand();
+        command.Connection = conn;
+        command.CommandText = "SELECT * FROM Animal";
+        
+        //wykonanie commanda
+        var reader = command.ExecuteReader();
+
+        var animals = new List<Animal>();
+
+        int IdAnimalOrdinal = reader.GetOrdinal("IdAnimal");
+        int NameOrdinal = reader.GetOrdinal("Name");
+
+        while (reader.Read())
+        {
+            animals.Add(new Animal()
+            {
+                IdAnimal = reader.GetInt32(IdAnimalOrdinal),
+                Name = reader.GetString(NameOrdinal)
+            });
+        }
+        
+        
+        return Ok(animals);
+    }
+
+    [HttpPost]
+    public IActionResult AddAnimal(AddAnimalReq animal)
+    {
+        //otwieramy polaczenie do bazy
+        SqlConnection conn = new SqlConnection(_config.GetConnectionString("Default"));
+        conn.Open();
+        //definicja commanda
+        SqlCommand command = new SqlCommand();
+        command.Connection = conn;
+        command.CommandText = $"INSERT INTO Animal VALUES(@animalName, @animalDescription, @animalCategory, @animalArea)";
+        command.Parameters.AddWithValue("@animalName", animal.Name);
+        command.Parameters.AddWithValue("@animalDescription", animal.Description);
+        command.Parameters.AddWithValue("@animalCategory", animal.Category);
+        command.Parameters.AddWithValue("@animalArea", animal.Area);
+        //wykonanie commanda
+        command.ExecuteNonQuery();
+        return Created("", null);
+    }
+}
---
+++ Models/DTOs/AddAnimalReq.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace apbd_cw06.Models.DTOs;
+
+public class AddAnimalReq
+{
+    [Required]
+    public string Name { get; set; }
+    public string? Description { get; set; }
+    [Required]
+    public string Category { get; set; }
+    [Required]
+    public string Area { get; set; }
+}
